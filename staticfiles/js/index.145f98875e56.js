3707ec960c2b465b55e9ce16f3065217f54cb052
Leus <nikleus525@gmail.com>
2023-12-30 21:42:54 +0100

    made prod usable

---
+++ staticfiles/js/index.145f98875e56.js
@@ -0,0 +1,20 @@
+let arr2 = document.getElementsByClassName('title');
+let arr3 = document.getElementsByClassName('text-secondary');
+const searchInput = document.querySelector('[data-search]')
+const arr = Array.from(arr2);
+const sec = Array.from(arr3);
+i = -1
+ducks = arr.map(duck => {
+    i++
+    return { name: duck.innerText, type: sec[i].innerText, par: duck.offsetParent.parentElement }
+})
+console.log(ducks);
+searchInput.addEventListener("input", e => {
+    const value = e.target.value.toLowerCase()
+    ducks.forEach(duck => {
+        const isVis = duck.name.toLowerCase().includes(value) || duck.type.toLowerCase().includes(value)
+        duck.par.classList.toggle("hide", !isVis)
+    })
+})
+window.onscroll = function() {getMyStickyHeader()};
+
